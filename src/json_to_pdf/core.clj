1da533ccafcad08a2c618665084417bb60396c4d
Yogthos <dmitri.sotnikov@gmail.com>
2012-12-11 10:06:11 -0500

    fixed parsing keywords

--- src/json_to_pdf/core.clj
+++ src/json_to_pdf/core.clj
@@ -8,8 +8,10 @@
 
 (defn -writePDFToStream [^java.io.InputStream in ^java.io.OutputStream out]
   (with-open [r (new java.io.PushbackReader (clojure.java.io/reader in))]
-    (clj-pdf.core/to-pdf (fn [r] (clojure.data.json/read r :eof-error? false)) r out)))
+    (clj-pdf.core/to-pdf (fn [r] (clojure.data.json/read r :eof-error? false :key-fn keyword)) r out)))
 
 
 (defn -writePDFToFile [^String json ^String filename]
     (-writePDFToStream (new java.io.ByteArrayInputStream (.getBytes json)) (new java.io.FileOutputStream filename)))
+
+

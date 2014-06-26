c152523c3d20b2ee1fcbf6d50115c74dc6087008
Yogthos <dmitri.sotnikov@gmail.com>
2014-06-25 20:56:42 -0400

    updated streaming API

--- src/json_to_pdf/core.clj
+++ src/json_to_pdf/core.clj
@@ -1,14 +1,15 @@
 (ns json-to-pdf.core
   (:require [clj-pdf.core :refer [pdf]]
-            [cheshire.core :refer [parse-string]])
-  (:import java.io.OutputStream)
+            [cheshire.core :refer [parse-string parse-stream]]
+            [clojure.java.io :refer [reader]])
+  (:import [java.io InputStream OutputStream])
   (:gen-class
    :name org.yogthos.JsonPDF
    :methods [#^{:static true} [writeToFile [String String] void]
-             #^{:static true} [writeToStream [String java.io.OutputStream] void]]))
+             #^{:static true} [writeToStream [java.io.InputStream java.io.OutputStream] void]]))
 
 (defn -writeToFile [^String json ^String filename]
  (pdf (parse-string json true) filename))
 
-(defn -writeToStream [^String json ^OutputStream out]
- (pdf (parse-string json true) out))
+(defn -writeToStream [^InputStream json ^OutputStream out]
+ (pdf (parse-stream (reader json) true) out))

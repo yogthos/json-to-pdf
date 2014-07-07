4ab567ceaa8354abe0fd2b4531fb8016384d7764
Yogthos <dmitri.sotnikov@gmail.com>
2014-07-07 17:24:31 -0400

    updated clj-pdf dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject json-to-pdf "0.6.7"
+(defproject json-to-pdf "0.6.8"
   :description "a JVM library for generating PDF documents given JSON markup"
   :url "https://github.com/yogthos/json-to-pdf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-pdf "1.11.19"]
+                 [clj-pdf "1.11.20"]
                  [cheshire "5.3.1"]
                  [org.apache.xmlgraphics/batik-script "1.7"]]
   :aot  [json-to-pdf.core])

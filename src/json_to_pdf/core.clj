aaf06c8360385ce3dab61633d86701ecf9d94a8b
Yogthos <dmitri.sotnikov@gmail.com>
2015-04-20 08:46:10 -0400

    added a fix for handling pagebreaks

--- src/json_to_pdf/core.clj
+++ src/json_to_pdf/core.clj
@@ -1,5 +1,6 @@
 (ns json-to-pdf.core
   (:require [clj-pdf.core :refer [pdf]]
+            [clojure.walk :refer [prewalk]]
             [cheshire.core :refer [parse-string parse-stream]]
             [clojure.java.io :refer [reader]])
   (:import [java.io InputStream OutputStream])
@@ -8,8 +9,14 @@
    :methods [#^{:static true} [writeToFile [String String] void]
              #^{:static true} [writeToStream [java.io.InputStream java.io.OutputStream] void]]))
 
+(defn fix-pagebreaks [doc]
+  (prewalk #(if (= % ["pagebreak"]) [:pagebreak] %) doc))
+
+(defn parse [json parser out]
+  (-> json (parser true) fix-pagebreaks (pdf out)))
+
 (defn -writeToFile [^String json ^String filename]
- (pdf (parse-string json true) filename))
+  (parse json parse-string filename))
 
 (defn -writeToStream [^InputStream json ^OutputStream out]
- (pdf (parse-stream (reader json) true) out))
+  (parse json parse-stream out))

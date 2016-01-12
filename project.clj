16b9fc8fa1596fea3e849b15b93f9c013df9cb27
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-01-12 14:13:22 -0500

    updated to latest clj-pdf

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-pdf "2.0.9"]
+                 [clj-pdf "2.1.7"]
                  [cheshire "5.5.0"]
                  [org.apache.xmlgraphics/batik-script "1.7"]]
   :aot  [json-to-pdf.core])

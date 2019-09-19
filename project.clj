03130294989c7e73a17bf8474105cd1fe69b665a
shyam sunder k <surya46584@gmail.com>
2019-09-19 19:15:09 -0400

    bump lein version to 0.8.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject json-to-pdf "0.8.1"
+(defproject json-to-pdf "0.8.2"
   :description "a JVM library for generating PDF documents given JSON markup"
   :url "https://github.com/yogthos/json-to-pdf"
   :license {:name "Eclipse Public License"
